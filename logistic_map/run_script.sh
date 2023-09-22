1aca716ddbb811de7692c6d010f083e16c26f263
Pietro Novelli <pietro.novelli@iit.it>
2023-09-22 19:11:00 +0200

    Edited run script

--- logistic_map/run_script.sh
+++ logistic_map/run_script.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=16:ngpus=1:mem=32gb
-#PBS -l walltime=24:00:00
-#PBS -q gpu
-#PBS -N ordered_MNIST-DPNets
-#PBS -o /work/pnovelli/dp_examples/ordered_MNIST/logs/out.log
-#PBS -e /work/pnovelli/dp_examples/ordered_MNIST/logs/err.log
-
-module load miniconda3/mc3-py39
-
-source activate /projects/mlcompchem/mambaforge/envs/kooplearn
-# Change to the working directory
-cd /work/pnovelli/dp_examples/ordered_MNIST 
 #List containing the models to run
 MODELS=("Cheby-T", "VAMPNets", "DPNets", "DPNets-relaxed")
 
