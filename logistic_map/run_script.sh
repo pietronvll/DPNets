648e1fd4ded027d3c00a76397f759168910b6701
Pietro Novelli <pietro.novelli@iit.it>
2023-09-22 19:23:17 +0200

    Edited figs running simulations

--- logistic_map/run_script.sh
+++ logistic_map/run_script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #List containing the models to run
-MODELS=("Cheby-T", "VAMPNets", "DPNets", "DPNets-relaxed")
-
-for model_name in "${MODELS[@]}"; do
+for model_name in Cheby-T DPNets DPNets-relaxed; do
     python run.py --model="$model_name"
 done
 # Wait for all instances to finish
