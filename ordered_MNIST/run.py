f3d9a3c872f61153efaf2fe56d96f171ebad0592
Pietro Novelli <pietro.novelli@iit.it>
2023-09-25 12:58:48 +0200

    Updating results

--- ordered_MNIST/run.py
+++ ordered_MNIST/run.py
@@ -23,7 +23,7 @@ experiment_path = Path(__file__).parent
 data_path = experiment_path / "data"
 ckpt_path = experiment_path / "ckpt"
 results_path = experiment_path / "results"
-configs = ml_confs.from_file(experiment_path / "configs.yaml")
+configs = ml_confs.from_file(experiment_path / "configs.yaml", register_jax_pytree = False)
 
 # Logging
 logger = logging.getLogger("ordered_MNIST")
