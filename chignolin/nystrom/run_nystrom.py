e2a97e2c8bd1d983ea34b22e285fc55c3327c564
Pietro Novelli <pietro.novelli@iit.it>
2023-09-27 09:09:39 +0200

    Updated Script

--- chignolin/nystrom/run_nystrom.py
+++ chignolin/nystrom/run_nystrom.py
@@ -2,6 +2,7 @@ import os
 import pickle
 import time
 from pathlib import Path
+from typing import Optional
 
 import mdtraj
 import ml_confs
@@ -24,19 +25,23 @@ def tensor_size(t: torch.Tensor) -> int:
     return nbytes / 1e9
 
 
-def get_data(num_data_pts: int):
+def load_data():
     pwised = np.load(exp_path / "data/CLN025_heavy_pwisedist.npy")
-    num_data_pts = min(num_data_pts, pwised.shape[0] - 1)
     pwised = torch.from_numpy(pwised)
     # Data Normalization
-    feature_norm = torch.linalg.norm(pwised, dim=0, keepdim=True)
-    feature_mean = torch.mean(pwised, dim=0, keepdim=True)
-    pwised -= feature_mean
-    pwised /= feature_norm
+    feature_mean = pwised.mean(dim=0, keepdim=True)
+    feature_std = pwised.std(dim=0, keepdim=True)
+    rsqrt_feature_dim = (pwised.shape[1]) ** -0.5
+    pwised = rsqrt_feature_dim * (pwised - feature_mean) / feature_std
+    return pwised
 
-    rnd_ids = np.random.choice(
-        np.arange(pwised.shape[0] - 1), num_data_pts, replace=False
-    )
+
+def make_dataset(train_pts: Optional[int] = None):
+    pwised = load_data()
+    if train_pts is None:
+        train_pts = pwised.shape[0] - 1
+    train_pts = min(train_pts, pwised.shape[0] - 1)
+    rnd_ids = np.random.choice(np.arange(pwised.shape[0] - 1), train_pts, replace=False)
     X = pwised[rnd_ids]
     Y = pwised[rnd_ids + 1]
     del pwised
@@ -44,38 +49,69 @@ def get_data(num_data_pts: int):
     return X, Y
 
 
+def fit_and_evd(X, Y, kind: str, **kw):
+    print(f"Training {kind.upper()}")
+    start = time.time()
+    est = train_est(X, Y, kind=kind, **kw)
+    fit_time = time.time() - start
+    print(f"Training took {fit_time:.1f} seconds")
+    print("Computing EVD")
+    data = load_data()
+    start = time.time()
+    evals, _left_fn, _right_fn = est.eigenfunctions()
+    evals = evals.resolve_conj().numpy()
+    efun_left = _left_fn(data).resolve_conj().numpy()
+    efun_right = _right_fn(data).resolve_conj().numpy()
+    evd_time = time.time() - start
+    print(f"EVD took {evd_time:.1f} seconds")
+
+    report = {
+        "model": model,
+        "fit_time": fit_time,
+        "evd_time": evd_time,
+        "eigenvalues": evals,
+        "left_eigenfunctions": efun_left,
+        "right_eigenfunctions": efun_right,
+    }
+    data_path = exp_path / "data/nystrom_evd"
+    if not data_path.exists():
+        data_path.mkdir(parents=True)
+    with open(data_path / f"{model.upper()}_report.pkl", "wb") as f:
+        pickle.dump(report, f)
+    del report
+    del data
+
+
+def median_heuristic(num_sample_points: int = 10000):
+    data = load_data()
+    rnd_ids = np.random.choice(
+        np.arange(data.shape[0] - 1), num_sample_points, replace=False
+    )
+    data = data[rnd_ids]
+    dists = torch.cdist(data, data)
+    i, j = torch.triu_indices(dists.shape[0], dists.shape[1])
+    dists = dists[i, j]
+    return dists.median().item()
+
+
 if __name__ == "__main__":
+    ls = median_heuristic()
+    print(f"Length-scale from the Median Heuristics: {ls:.3f}")
     model_kw = {
         "pcr": {
             "num_components": configs.feature_dim,
             "M": configs.inducing_points,
-            "kernel": GaussianKernel(configs.ls, opt=FalkonOptions(use_cpu=True)),
+            "kernel": GaussianKernel(ls, opt=FalkonOptions(use_cpu=True)),
             "svd_solver": "full",
         },
         "rrr": {
             "penalty": 1e-9,
             "num_components": configs.feature_dim,
             "M": configs.inducing_points,
-            "kernel": GaussianKernel(configs.ls, opt=FalkonOptions(use_cpu=True)),
+            "kernel": GaussianKernel(ls, opt=FalkonOptions(use_cpu=True)),
         },
     }
-    X, Y = get_data(num_data_pts=configs.num_data_pts)
-    print("Data Loaded")
-    # Data shuffling
-    rng = np.random.default_rng()
     # Train Nystrom
     for model in model_kw.keys():
-        print(f"Training {model.upper()}")
-        start = time.time()
-        est = train_est(X, Y, kind=model, **model_kw[model])
-        t_elapsed = time.time() - start
-        print(f"Training took {t_elapsed:.1f} seconds")
-        v, lf, rf = evals, efun_left, efun_right = est.eigenfunctions()
-        report = {
-            "model": model,
-            "fit_time": t_elapsed,
-            "eigenvalues": v.detach().resolve_conj().numpy(),
-            "estimator": est,
-        }
-        with open(exp_path / f"ckpt/nystrom_{model}.pkl", "wb") as f:
-            pickle.dump(report, f)
+        kw = model_kw[model]
+        fit_and_evd(*make_dataset(configs.num_data_pts), **kw)
