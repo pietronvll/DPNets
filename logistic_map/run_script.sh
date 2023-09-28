14b33a79d711f0af446c9f8582fb2c25a851db97
Pietro Novelli <pietro.novelli@iit.it>
2023-09-28 09:40:16 +0200

    Repo Cleaning

--- logistic_map/run_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#List containing the models to run
-for model_name in Cheby-T VAMPNets DPNets DPNets-relaxed NoiseKernel; do
-    python run.py --model="$model_name"
-done
-# Wait for all instances to finish
-wait
