c3452b43d01c609815ccdde042e6654cb65dde30
Pietro Novelli <pietro.novelli@iit.it>
2023-09-28 17:18:29 +0200

    Purged results

--- fluid/run_franklin.sh
+++ fluid/run_franklin.sh
@@ -5,7 +5,7 @@
 #PBS -N fluid-DPNets
 #PBS -j oe
 #PBS -o /work/pnovelli/dp_examples/fluid/logs/output.log
-#PBS -J 10-19
+#PBS -J 0-9
 #PBS -r y
 
 module load miniconda3/mc3-py39
@@ -14,7 +14,7 @@ source activate /projects/mlcompchem/mambaforge/envs/kooplearn
 # Change to the working directory
 cd /work/pnovelli/dp_examples/fluid 
 
-for model_name in VAMPNets DPNets DPNets-relaxed; do
+for model_name in DynamicalAE ConsistentAE; do
     python run.py --model="$model_name" --rngseed=$PBS_ARRAY_INDEX
 done
 # Wait for all instances to finish
