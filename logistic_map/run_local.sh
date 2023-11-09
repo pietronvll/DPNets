9374568a1f3c004354483d9866d099a62014b9d2
Pietro Novelli <pietro.novelli@iit.it>
2023-11-09 15:47:15 +0100

    Testing

--- logistic_map/run_local.sh
+++ logistic_map/run_local.sh
@@ -1,6 +1,6 @@
 for FDIM in {6..10}; do
     for model_name in Cheby-T VAMPNets DPNets DPNets-relaxed NoiseKernel; do
-        python run.py --model="$model_name" --fdim=$PBS_ARRAY_INDEX
+        python run.py --model="$model_name" --fdim=$FDIM
     done
 done
 # Wait for all instances to finish
