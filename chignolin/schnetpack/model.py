3b47645c7d82b180e5aa3e710ffc87edc920c79e
Pietro Novelli <pietronvll@gmail.com>
2023-11-16 11:36:53 +0100

    Refactor DeepGraphKernel and SchNet classes

--- chignolin/schnetpack/model.py
+++ chignolin/schnetpack/model.py
@@ -1,5 +1,14 @@
+import logging
+from copy import deepcopy
+
 import ml_confs
+import schnetpack
 import torch
+from kooplearn.nn.functional import (
+    log_fro_metric_deformation_loss,
+    relaxed_projection_score,
+    vamp_score,
+)
 from lightning import LightningModule
 
 
@@ -9,100 +18,93 @@ class DeepGraphKernel(LightningModule):
         configs: ml_confs.Configs,
         n_atoms: int,
         optimizer: torch.optim.Optimizer,
-        optim_kargs: dict[str, Any],
+        use_relaxed_loss: bool = False,
+        metric_deformation_loss_coefficient: float = 1.0,  # That is, the parameter γ in the paper.
+        optimizer_kwargs={},
     ):
         super().__init__()
+
+        self.save_hyperparameters(
+            ignore=["encoder", "optimizer_fn", "kooplearn_feature_map_weakref"]
+        )
+        _tmp_opt_kwargs = deepcopy(optimizer_kwargs)
+        if "lr" in _tmp_opt_kwargs:  # For Lightning's LearningRateFinder
+            self.lr = _tmp_opt_kwargs.pop("lr")
+            self.opt_kwargs = _tmp_opt_kwargs
+        else:
+            self.lr = 1e-3
+            self.opt_kwargs = {}
+            logging.warning(
+                "No learning rate specified. Using default value of 1e-3. You can specify the learning rate by passing it to the optimizer_kwargs argument."
+            )
+
         self.schnet = SchNet(configs)
         self.n_atoms = n_atoms
         self.configs = configs
         self.optimizer = optimizer
-        self.optim_kwargs = optim_kargs
-        self.save_hyperparameters()
 
     def forward(self, inputs: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
         return self.schnet(inputs)
 
     def training_step(self, batch, batch_idx):
         features = self.schnet(batch)["scalar_representation"]
-        batch_size = self.configs.training.batch_size
-        phi = features.reshape(
-            batch_size * 2, self.n_atoms, self.configs.network.n_final_features
-        )
-        phi_kme = phi.mean(dim=1)  # Linear kernel mean embedding
-        phi_X = phi_kme[::2]
-        phi_Y = phi_kme[1::2]
+        features = features.reshape(-1, self.n_atoms, self.configs.n_final_features)
+        features = features.mean(dim=1)  # Linear kernel mean embedding
+        encoded_X = features[::2]
+        encoded_Y = features[1::2]
 
-        norm_cov = (batch_size) ** -1.0
-        in_cov = norm_cov * (phi_X.T @ phi_X)
-        out_cov = norm_cov * (phi_Y.T @ phi_Y)
-        cross_cov = norm_cov * (phi_X.T @ phi_Y)
+        _norm = torch.rsqrt(torch.tensor(encoded_X.shape[0]))
+        encoded_X *= _norm
+        encoded_Y *= _norm
 
-        S = DPNets_S(in_cov, out_cov, cross_cov)
-        reg = vn_metric_reg(in_cov, reg=self.configs.training.reg)
-        if batch_idx % self.configs.training.log_every == 0:
-            P = DPNets_P(in_cov, out_cov, cross_cov, epsilon=0)
-            cond = torch.linalg.cond(in_cov)
-            logging_dict = {
-                "train/S": S,
-                "train/P": P,
-                "train/cond": cond,
-                "train/reg": reg,
-            }
-            self.log_dict(logging_dict)
-        return -S + reg
+        cov_X = torch.mm(encoded_X.T, encoded_X)
+        cov_Y = torch.mm(encoded_Y.T, encoded_Y)
+        cov_XY = torch.mm(encoded_X.T, encoded_Y)
 
-    def validation_step(self, batch, batch_idx):
-        features = self.schnet(batch)["scalar_representation"]
-        batch_size = self.configs.training.batch_size
-        phi = features.reshape(
-            batch_size * 2, self.n_atoms, self.configs.network.n_final_features
-        )
-
-        phi_kme = phi.mean(dim=1)  # Linear kernel mean embedding
-        phi_X = phi_kme[::2]
-        phi_Y = phi_kme[1::2]
-
-        norm_cov = (batch_size) ** -1.0
-        in_cov = norm_cov * (phi_X.T @ phi_X)
-        out_cov = norm_cov * (phi_Y.T @ phi_Y)
-        cross_cov = norm_cov * (phi_X.T @ phi_Y)
-
-        S = DPNets_S(in_cov, out_cov, cross_cov)
-        reg = vn_metric_reg(in_cov, reg=self.configs.training.reg)
-        P = DPNets_P(in_cov, out_cov, cross_cov, epsilon=0)
-        cond = torch.linalg.cond(in_cov)
-        logging_dict = {"val/S": S, "val/P": P, "val/cond": cond, "val/reg": reg}
-        self.log_dict(logging_dict, sync_dist=True)
+        metrics = {}
+        # Compute the losses
+        if self.hparams.use_relaxed_loss:
+            svd_loss = -1 * relaxed_projection_score(cov_X, cov_Y, cov_XY)
+            metrics["train/relaxed_projection_score"] = -1.0 * svd_loss.item()
+        else:
+            svd_loss = -1 * vamp_score(cov_X, cov_Y, cov_XY, schatten_norm=2)
+            metrics["train/projection_score"] = -1.0 * svd_loss.item()
+        if self.hparams.metric_deformation_loss_coefficient > 0.0:
+            metric_deformation_loss = 0.5 * (
+                log_fro_metric_deformation_loss(cov_X)
+                + log_fro_metric_deformation_loss(cov_Y)
+            )
+            metric_deformation_loss *= self.hparams.metric_deformation_loss_coefficient
+            metrics["train/metric_deformation_loss"] = metric_deformation_loss.item()
+            svd_loss += metric_deformation_loss
+        metrics["train/total_loss"] = svd_loss.item()
+        self.log_dict(metrics, on_step=True, prog_bar=True, logger=True)
+        return svd_loss
 
     def configure_optimizers(self):
-        optimizer = self.optimizer(params=self.parameters(), **self.optim_kwargs)
-        return optimizer
+        kw = self.opt_kwargs | {"lr": self.lr}
+        return self._optimizer(self.parameters(), **kw)
 
 
 class SchNet(schnetpack.model.AtomisticModel):
-    def __init__(self, configs: JSONNamespace):
+    def __init__(self, configs: ml_confs.Configs):
         super().__init__(
             input_dtype_str="float32",
             postprocessors=None,
             do_postprocessing=False,
         )
-        self.cutoff = configs.data.cutoff
+        self.cutoff = configs.cutoff
         self.pwise_dist = schnetpack.atomistic.PairwiseDistances()
         self.radial_basis = schnetpack.nn.GaussianRBF(
-            n_rbf=configs.network.n_rbf, cutoff=self.cutoff
+            n_rbf=configs.n_rbf, cutoff=self.cutoff
         )
         self.net = schnetpack.representation.SchNet(
-            n_atom_basis=configs.network.n_atom_basis,
-            n_interactions=configs.network.n_interactions,
+            n_atom_basis=configs.n_atom_basis,
+            n_interactions=configs.n_interactions,
             radial_basis=self.radial_basis,
             cutoff_fn=schnetpack.nn.CosineCutoff(self.cutoff),
         )
-        self.final_lin = torch.nn.Linear(
-            configs.network.n_atom_basis, configs.network.n_final_features
-        )
-        self.batch_norm = torch.nn.BatchNorm1d(
-            configs.network.n_final_features, affine=False
-        )
+        self.final_lin = torch.nn.Linear(configs.n_atom_basis, configs.n_final_features)
 
     def forward(self, inputs: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
         inputs = self.pwise_dist(inputs)
@@ -110,11 +112,4 @@ class SchNet(schnetpack.model.AtomisticModel):
         inputs["scalar_representation"] = self.final_lin(
             inputs["scalar_representation"]
         )
-        # n_atoms = inputs[schnetpack.properties.n_atoms][0] #Assuming constant number of atoms across the batch
-        # n_final_features = inputs['scalar_representation'].shape[-1]
-        # inputs['scalar_representation'] = self.batch_norm(
-        #     inputs['scalar_representation'].view(-1, n_atoms, n_final_features).permute(0, 2, 1)
-        # ).permute(0, 2, 1).reshape(-1, n_final_features)
-        # inputs['scalar_representation'] =  inputs['scalar_representation']*torch.rsqrt(torch.tensor(n_final_features, dtype=torch.float32))
-        return inputs
         return inputs
